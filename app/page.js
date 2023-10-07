8be8f296206ddf1db887dac87fa1dd8c972d09f8
Hectorfr <hector.freard@gmail.com>
2023-10-07 18:14:11 +0200

    v1

--- app/page.js
+++ app/page.js
@@ -1,113 +1,112 @@
 import Image from 'next/image'
 
+import cinmoon from "../public/civilian-in-the-moon.jpeg";
+
+import { AiFillYoutube } from 'react-icons/ai';
+import { SiNasa, SiYourtraveldottv} from 'react-icons/si';
+import { FaSpaceShuttle } from 'react-icons/fa';
+import { BsMoonStars } from 'react-icons/bs';
+
 export default function Home() {
   return (
+    <div className='bg-gray-900'>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
+      <link rel="icon" href="/icon.ico" />
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
+        <p className="font-mono fixed text-gray-200 left-0 top-0 flex w-full justify-center  pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
+          The moon is at the tip of your fingers&nbsp;
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="h-screen">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+          src={cinmoon}
+          alt="Moon logo"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          />
       </div>
+          <section className="pb-10">
+            <div className="text-center">
+              <h1 className="text-5xl text-yellow-300 md:text-6xl">
+                Tourismoon 
+              </h1>
+              <h3 className="text-2xl py-2 text-yellow-200 md:text-3xl">
+                Your space dream come true
+              </h3>
+              <p className="text-md py-5 leading-8 max-w-lg mx-auto p-10 text-yellow-100 md:text-xl">
+                What you thought was impossible is alredy here, <span className="text-yellow-200">space travel</span> is at your reach. 
+              </p>
+            </div>
+          </section>
+          <section> 
+            <div id="projects">
+              <h3 className="text-3xl text-center text-yellow-300 py-2">Travel options</h3>
+            </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <div className="xl:flex gap-10">
+                <div className="mx-center text-center shadow-xl p-10 rounded-xl my-10 border-yellow-800">
+                  <h2 className="text-lg pt-8 pb-2 font-bold text-yellow-600">Standard experience</h2>
+                  <p className="py-2 p-10 text-yellow-200">
+                    In this project I have put to the test my front-end skills, where I have a created a website from scracth,
+                    this portfolio showcases my proyects and as such it has to recieve updates whenever I engage or finish one.
+                    Also doing small changes to correct bugs and smooth out the user experience with new tecnologies.
+                  </p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                  <h4 className="py-2 text-yellow-600">Video examples</h4>
+                  <div className="text-5xl text-yellow-600 flex justify-center gap-10 py-3">
+                    <a href="https://youtube.com" target="_blank" rel="noreferrer"><AiFillYoutube/></a>
+                  </div>
+              </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+              <div>
+                GET ME TO THE MOON
+              </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              <div className="xl:flex gap-10">
+                <div className="mx-center text-center shadow-xl p-10 rounded-xl my-10 border-yellow-800">
+                  <h2 className="text-lg pt-8 pb-2 font-bold text-yellow-600">Luxury experience</h2>
+                  <p className="py-2 p-10 text-yellow-200">
+                    In this project I have put to the test my front-end skills, where I have a created a website from scracth,
+                    this portfolio showcases my proyects and as such it has to recieve updates whenever I engage or finish one.
+                    Also doing small changes to correct bugs and smooth out the user experience with new tecnologies.
+                  </p>
+
+                  <h4 className="py-2 text-yellow-600">Video examples</h4>
+                  <div className="text-5xl text-yellow-600 flex justify-center gap-10 py-3">
+                    <a href="https://youtube.com" target="_blank" rel="noreferrer"><AiFillYoutube/></a>
+                  </div>
+              </div>
+              </div>
+            </div>
+
+            <div className="gap-10 mx-center text-center shadow-xl p-10 rounded-xl my-10 border-yellow-800 mx-xl:flex">
+            <h4 className="py-2 font-bold text-white">Partnership</h4>
+                <div className="text-8xl text-yellow-600 flex justify-center gap-10 py-3">
+                  <a href="https://www.nasa.gov/" target="_blank" rel="noreferrer"><SiNasa/></a>
+                  <a href="https://www.spacex.com/" target="_blank" rel="noreferrer"><SiYourtraveldottv/></a>
+                  <a href="https://www.nasa.gov/" target="_blank" rel="noreferrer"><FaSpaceShuttle/></a>
+                </div>
+            </div>
+          </section>
     </main>
+
+    <footer className="border-t-4 border-yellow-900 ">
+          <div className="w-full mx-auto max-w-screen-xl p-4 text-yellow-200 md:flex md:items-center md:justify-between">
+            <span className="text-sm sm:text-center">© 2023 <a className="hover:underline">Tourismoon</a>. All Rights Reserved.
+            </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium dark:text-gray-400 sm:mt-0">
+              <li>
+                  <a href="#" className="mr-4 hover:underline md:mr-6 ">About us</a>
+              </li>
+              <li>
+                  <a href="https://www.linkedin.com/in/hector-fr/" target="_blank" rel="noreferrer" className="hover:underline"></a>
+              </li>
+          </ul>
+          </div>
+
+        </footer>
+
+    </div>
   )
 }
